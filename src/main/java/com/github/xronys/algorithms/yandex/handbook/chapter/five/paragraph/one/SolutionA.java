e305519f4f8ae0413b65dc1d6e44cfbfe8deec42
Igor Kosachev <kronyys@gmail.com>
2023-06-29 18:14:13 +0300

    Yandex algorithms handbook, chapter 5, paragraph 1, task A solved

---
+++ src/main/java/com/github/xronys/algorithms/yandex/handbook/chapter/five/paragraph/one/SolutionA.java
@@ -0,0 +1,26 @@
+package com.github.xronys.algorithms.yandex.handbook.chapter.five.paragraph.one;
+
+import java.io.*;
+
+public class SolutionA {
+    public static long solve( int n ) {
+        if (n <= 1)
+            return n;
+        long previous = 0;
+        long current = 1;
+        for (int i = 0; i < n - 1; i++) {
+            long oldPrevious = previous;
+            previous = current;
+            current = oldPrevious + previous;
+        }
+        return current;
+    }
+
+    public static void main( String[] args ) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int n = Integer.parseInt(reader.readLine());
+            writer.write(String.valueOf(solve(n)));
+        }
+    }
+}
---
+++ src/main/java/com/github/xronys/algorithms/yandex/handbook/chapter/five/paragraph/one/SolutionB.java
@@ -0,0 +1,4 @@
+package com.github.xronys.algorithms.yandex.handbook.chapter.five.paragraph.one;
+
+public class SolutionB {
+}
